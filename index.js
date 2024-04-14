5a941875482b927a383e2166ead941a6a8e99cd7
Peter <peterkastias@hotmail.com>
2024-04-13 21:14:26 -0400

    Impleneted and completed exercises 3.1-3.6

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ let persons = [
   }
 ]
 
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id))
+  : 0
+  return maxId + 1
+}
+
 app.get('/', (request, response) => {
   response.send('<h1> Helldddo Wodddrld! </h1>')
 })
@@ -38,6 +45,50 @@ app.get('/info', (request, response) => {
   const phonebookEntries = persons.length
 
   response.send(`<p>Phonebook has info for ${phonebookEntries} people</p><p>${currentTime}</p>`);
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+       error: ' THIS IS NOT A CORRECT FORMAT JOSH!!! '
+    })
+  }
+  
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(409).json ({
+      error: 'Name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+
 })
 
 const PORT = 3001
